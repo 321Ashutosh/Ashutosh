7167197503e296a9f704c2f6d09f89cfb054e17f
321Ashutosh <108077736+321Ashutosh@users.noreply.github.com>
2022-06-23 20:13:00 +0530

    Add files via upload

---
+++ Assigment - 4    (19-06-2022 ).cpp
@@ -0,0 +1,95 @@
+				        	/*	program - 1
+         write a C program to print Hello Students on the screen. */
+/*
+#include<stdio.h>
+int main()
+{
+	printf("Hello Student");
+ } */
+ 
+					        /* program - 2
+	 write a C program to print Hello on the frist line and Students in the sccond. */
+/*
+#include<stdio.h>
+int main()
+{
+	printf("Hello\nStudent");
+ } */
+ 
+ 
+ 							/* program -3 
+	 	write a C program to print "My SirG" on the Screen */
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("\"My SirG\"");
+	getch();
+ } 
+ */
+ 
+ 								
+                              /* program -4
+	 	write a C program to print "Teacher's Day" on the Screen */
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("\"Teacher\'s Day\"");
+	getch();
+ } */								
+ 
+ 								 /* program -5
+	 	write a C program to print \n on the Screen */
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("\\n");
+	getch();
+ } */								
+ 
+ 
+ 								 /* program-6
+	 		write a C program to print %d on the Screen */
+/*
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("%%d");
+	getch();
+ } 
+ */
+                                 /*program-7
+ Write a C program contaning declaration of three variables (of type int,char and float),
+ also assign some values to them and print values of all theree variables using single 
+ printf(). */
+/* 
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=89;
+	char ch='D';
+	float c=1.22;
+	printf("%d %c %f",a,ch,c);
+	getch();
+ } */
+ 
+ 									/*program-9
+Write a C program to print character stored in a char variable,also print its ASCII code.*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char ch;
+	printf("Enter a character:- ");
+	scanf("%c",&ch);
+	printf("\n The ASCII value of the character variable is:- %d",ch);
+	getch();
+ } 								
